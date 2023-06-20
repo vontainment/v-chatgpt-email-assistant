c0ad4020f809eb6501a6a915f9faa11c2d371e39
Nikolai X. Shadeauxs <services@vontainment.com>
2023-06-19 21:00:24 -0400

    beta update
    add signature

--- beta/imapsieve_globalreply.py
+++ beta/imapsieve_globalreply.py
@@ -50,17 +50,19 @@ def get_config_values(nixuser):
     with open(f'/home/{nixuser}/mail/ai.conf', 'r') as f:
         ai_conf_content = f.read()
 
-    # Use regular expressions to find the user key and instructions
+    # Use regular expressions to find the user key, instructions, and signature
     userkey_match = re.search(r'userkey="([^"]*)"', ai_conf_content)
     instructions_match = re.search(r'instructions="([^"]*)"', ai_conf_content)
+    signature_match = re.search(r'signature=|||([^|||]*)|||', ai_conf_content)
 
-    if userkey_match and instructions_match:
-        # Return the matched user key and instructions
-        return userkey_match.group(1), instructions_match.group(1)
+    if userkey_match and instructions_match and signature_match:
+        # Return the matched user key, instructions, and signature
+        return userkey_match.group(1), instructions_match.group(1), signature_match.group(1)
     else:
-        raise Exception("Could not find userkey or instructions in ai.conf")
+        raise Exception("Could not find userkey, instructions, or signature in ai.conf")
 
-userkey, instructions = get_config_values(nixuser)
+
+signature, userkey, instructions = get_config_values(nixuser)
 
 def decode_mail(email_data):
     msg = email.message_from_bytes(email_data)
@@ -90,7 +92,6 @@ def send_to_ai(from_name, subject, body):
 
     chat_models = 'gpt-3.5-turbo'
 
-    system_message = "You are an AI and are tasked with writing replies to emails. Write your replies as if you were the human to whom the email was sent and in the following format:\nHello FROM NAME,\n\nYOUR REPLY\n\nBest regards"
     system_message = f"{instructions}"
     user_message = f"This email is from:{from_name}. This email has a subject of: {subject}. This email's body is: {body}"
 
@@ -103,16 +104,17 @@ def send_to_ai(from_name, subject, body):
     )
     return result.choices[0].message['content']
 
-def create_reply(from_email, subject, body, new_msg, to_email):
+def create_reply(from_email, subject, body, new_msg, to_email, signature):
     re_subject = "Re: " + subject
-    text = new_msg + "\n\n" + body
+    text = new_msg + "<br><br>" + signature
 
-    msg = MIMEMultipart()
+    msg = MIMEMultipart('alternative')
     msg['From'] = to_email
     msg['To'] = from_email
     msg['Subject'] = re_subject
 
-    msg.attach(MIMEText(text, 'plain'))
+    part = MIMEText(text, 'html')
+    msg.attach(part)
 
     # Specify the location of the user's draft directory in Dovecot
     draft_dir = f"/home/{nixuser}/mail/{domain}/{user_email}/.Drafts/cur"
@@ -129,7 +131,7 @@ def create_reply(from_email, subject, body, new_msg, to_email):
 def process_email(email_data):
     from_name, from_email, to_email, subject, body = decode_mail(email_data)
     ai_response = send_to_ai(from_name, subject, body)
-    create_reply(from_email, subject, body, ai_response, to_email)
+    create_reply(from_email, subject, body, ai_response, to_email, signature)
 
 email_data = sys.stdin.buffer.read()
 process_email(email_data)
